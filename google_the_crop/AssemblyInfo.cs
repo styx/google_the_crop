ef9c91675d20c4fb8fa5bc9618192548446bd491
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-05-08 15:03:17 +0300

    Update metadata

--- google_the_crop/AssemblyInfo.cs
+++ google_the_crop/AssemblyInfo.cs
@@ -2,10 +2,10 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using Mono.Addins;
 
-[assembly: Addin("google_the_crop", "1.0", Category = "Other")]
-[assembly: AddinName ("Google for selected part of image")]
+[assembly: Addin("google_the_crop", "1.1", Category = "Other")]
+[assembly: AddinName ("Google selected part of image")]
 [assembly: AddinDescription ("Google for selected part of image without any Q!")]
-[assembly: AddinDependency ("Pinta", "1.5")]
+[assembly: AddinDependency ("Pinta", "1.6")]
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
@@ -15,7 +15,7 @@ using Mono.Addins;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("by.styx")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("styx")]
+[assembly: AssemblyCopyright("Mikhail Pabalavets")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,7 +23,7 @@ using Mono.Addins;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
